6514a408f380e13334f55c8d23b086e9ff907a64
Peter Abeles <peter.abeles@gmail.com>
2013-04-10 15:39:35 -0400

    - All unit tests pass now - Candidate for v0.3

--- test/org/ddogleg/stats/TestUtilGaussian.java
+++ test/org/ddogleg/stats/TestUtilGaussian.java
@@ -20,7 +20,8 @@ package org.ddogleg.stats;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Peter Abeles
@@ -44,21 +45,57 @@ public class TestUtilGaussian {
 
 	@Test
 	public void derivative1() {
-		fail("Implement");
+
+		// numerical derivative
+		double expected = deriv1(1.5, 2, 1.8);
+
+		assertEquals(expected,UtilGaussian.derivative1(1.5, 2, 1.8),1e-4);
 	}
 
 	@Test
 	public void derivative2() {
-		fail("Implement");
+		// numerical derivative.. test is very crude
+		double expected = deriv2(1.5, 2, 1.8);
+
+		assertEquals(expected, UtilGaussian.derivative2(1.5, 2, 1.8), 1e-3);
 	}
 
 	@Test
 	public void derivative3() {
-		fail("Implement");
+		// numerical derivative.. test is very crude
+		double expected = deriv3(1.5, 2, 1.8);
+
+		assertEquals(expected, UtilGaussian.derivative3(1.5, 2, 1.8), 1e-3);
 	}
 
 	@Test
 	public void derivative4() {
-		fail("Implement");
+		// numerical derivative.. test is very crude
+		double expected = deriv4(1.5, 2, 1.8);
+
+		assertEquals(expected, UtilGaussian.derivative4(1.5, 2, 1.8), 0.1);
+	}
+
+	public static double normal( double mu , double sigma ,double x ) {
+		double delta = x-mu;
+		double a = -delta*delta/(2*sigma*sigma);
+		double b = 2*Math.PI*sigma*sigma;
+
+		return (1.0/Math.sqrt(b))*Math.exp(a);
+	}
+
+	public static double deriv1( double mu , double sigma ,double x) {
+		return (normal(mu,sigma,x+1e-10) - normal(mu,sigma,x))/1e-10;
+	}
+	public static double deriv2( double mu , double sigma ,double x) {
+		return (deriv1(mu,sigma,x+1e-3) - deriv1(mu,sigma,x-1e-3))/2e-3;
+	}
+	public static double deriv3( double mu , double sigma ,double x) {
+		double d = 1e-1;
+		return (deriv2(mu,sigma,x+d) - deriv2(mu,sigma,x-d))/(2*d);
+	}
+	public static double deriv4( double mu , double sigma ,double x) {
+		double d = 0.1;
+		return (deriv3(mu, sigma, x + d) - deriv3(mu, sigma, x - d))/(2*d);
 	}
 }
