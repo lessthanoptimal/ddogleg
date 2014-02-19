b41c1ded27c6f9527065c14429c80728b58ef82a
Peter Abeles <peter.abeles@gmail.com>
2014-02-19 09:12:11 -0500

    - fixed bug in CircularQueue

--- src/org/ddogleg/struct/CircularQueue_I32.java
+++ src/org/ddogleg/struct/CircularQueue_I32.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2012-2014, Peter Abeles. All Rights Reserved.
+ *
+ * This file is part of DDogleg (http://ddogleg.org).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.ddogleg.struct;
 
 /**
@@ -95,7 +113,7 @@ public class CircularQueue_I32 {
 			int a[] = new int[ nextDataSize() ];
 
 			System.arraycopy(data,start,a,0,data.length-start);
-			System.arraycopy(data,0,a,data.length-start-1,start);
+			System.arraycopy(data,0,a,data.length-start,start);
 			start = 0;
 			data = a;
 		}
--- test/org/ddogleg/struct/TestCircularQueue_I32.java
+++ test/org/ddogleg/struct/TestCircularQueue_I32.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2012-2014, Peter Abeles. All Rights Reserved.
+ *
+ * This file is part of DDogleg (http://ddogleg.org).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.ddogleg.struct;
 
 import org.junit.Test;
@@ -152,11 +170,23 @@ public class TestCircularQueue_I32 {
 		assertEquals(4,alg.data[3]);
 		assertEquals(4,alg.size);
 
+		// grows with offset
+		alg.start = 1;
+		alg.data = new int[]{1,2,3};
+		alg.size = 3;
+		alg.add(4);
+		assertEquals(2,alg.data[0]);
+		assertEquals(3,alg.data[1]);
+		assertEquals(1,alg.data[2]);
+		assertEquals(4,alg.data[3]);
+		assertEquals(4,alg.size);
+
 		// wrap around case
 		alg.start = 1;
 		alg.size = 2;
 		alg.data = new int[3];
 		alg.add(10);
+		assertEquals(10,alg.data[0]);
 		assertEquals(10,alg.data[0]);
 		assertEquals(3,alg.size);
 
