971638eb59b67d4c88fa7d7c52d5bf0deb28631b
Peter Abeles <peter.abeles@gmail.com>
2015-07-06 12:39:16 -0700

    - Created UtilStatisticsQueue for computing statistics from GrowQueues

---
+++ src/org/ddogleg/stats/UtilStatisticsQueue.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2012-2015, Peter Abeles. All Rights Reserved.
+ *
+ * This file is part of DDogleg (http://ddogleg.org).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ddogleg.stats;
+
+import org.ddogleg.struct.GrowQueue_F64;
+
+/**
+ * @author Peter Abeles
+ */
+public class UtilStatisticsQueue {
+
+	public static double mean( GrowQueue_F64 list ) {
+		double total = 0;
+		for (int i = 0; i < list.size(); i++) {
+			total += list.data[i];
+		}
+
+		return total / list.size();
+	}
+
+	public static double variance( GrowQueue_F64 list , double mean ) {
+		double total = 0;
+		for (int i = 0; i < list.size(); i++) {
+			double d = list.data[i] - mean;
+			total += d*d;
+		}
+
+		return total / (list.size()-1);
+	}
+}
---
+++ test/org/ddogleg/stats/TestUtilStatisticsQueue.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2012-2015, Peter Abeles. All Rights Reserved.
+ *
+ * This file is part of DDogleg (http://ddogleg.org).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ddogleg.stats;
+
+import org.ddogleg.struct.GrowQueue_F64;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Peter Abeles
+ */
+public class TestUtilStatisticsQueue {
+	@Test
+	public void mean_F64() {
+		GrowQueue_F64 l = new GrowQueue_F64();
+		l.add(0);
+		l.add(1);
+		l.add(2);
+		l.add(3);
+		l.add(4);
+
+		double found = UtilStatisticsQueue.mean(l);
+		assertEquals(2,found,1e-8);
+	}
+
+	@Test
+	public void variance_F64() {
+		GrowQueue_F64 l = new GrowQueue_F64();
+		l.add(0);
+		l.add(1);
+		l.add(2);
+		l.add(3);
+		l.add(4);
+
+		double found = UtilStatisticsQueue.variance(l,2);
+		assertEquals(2.5,found,1e-8);
+	}
+}
