bbf5be22335805f69941e76522dd26e10af79afc
Peter Abeles <peter.abeles@gmail.com>
2023-02-09 09:14:14 -0800

    Added more packages

--- src/module-info.java
+++ src/module-info.java
@@ -1,4 +1,21 @@
-module org.ddogleg {
-//	requires transitive org.ejml;
+module ddogleg {
+	// https://github.com/gradlex-org/extra-java-module-info
+//	requires org.ejml;
+//	requires org.ejml.data;
+
 	exports org.ddogleg;
+	exports org.ddogleg.clustering;
+	exports org.ddogleg.combinatorics;
+	exports org.ddogleg.fitting.modelset;
+	exports org.ddogleg.fitting.modelset.distance;
+	exports org.ddogleg.fitting.modelset.lmeds;
+	exports org.ddogleg.fitting.modelset.ransac;
+	exports org.ddogleg.graph;
+	exports org.ddogleg.nn;
+	exports org.ddogleg.optimization;
+	exports org.ddogleg.rand;
+	exports org.ddogleg.solver;
+	exports org.ddogleg.stats;
+	exports org.ddogleg.struct;
+	exports org.ddogleg.util;
 }
